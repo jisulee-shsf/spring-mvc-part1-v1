0624a9ac35752c22779d778ca26a54ff64cb95a9
Jisu Lee <jisulee.shsf@gmail.com>
2024-06-03 18:57:48 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/study/springmvcpart1/SpringMvcPart1Application.java
+++ src/main/java/study/springmvcpart1/SpringMvcPart1Application.java
@@ -1,8 +1,11 @@
 package study.springmvcpart1;
 
+import jakarta.servlet.annotation.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringMvcPart1Application {
 
